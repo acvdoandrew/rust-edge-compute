b304e842d2f25110e7680dd60cfdab646013fb34
Andrew Acevedo Mirena <acvdoandrew@gmail.com>
2025-12-04 19:07:54 -0500

    build: configure hermetic protobuf compilation
    Adds protoc-bin-vendored to build.rs to ensure the project compiles on
    any system without requiring manual protoc installation.

---
+++ build.rs
@@ -0,0 +1,7 @@
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    std::env::set_var("PROTOC", protoc_bin_vendored::protoc_bin_path().unwrap());
+
+    tonic_build::compile_protos("proto/node.proto")?;
+
+    Ok(())
+}
